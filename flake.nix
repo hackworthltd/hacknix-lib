817de72e9e183daac3c1d0df289dfbca84768cd4
Drew Hess <src@drewhess.com>
2020-12-29 22:37:57 +0000

    Add some useful utilities for building configurations.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+
+    nix-darwin.url = github:LnL7/nix-darwin;
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
     flake-compat = {
       url = github:edolstra/flake-compat;
       flake = false;
@@ -13,7 +16,6 @@
   outputs =
     { self
     , nixpkgs
-    , flake-utils
     , ...
     }@inputs:
     let
--- nix/overlays/113-lib-flakes.nix
+++ nix/overlays/113-lib-flakes.nix
@@ -1,18 +1,5 @@
 final: prev:
 let
-  # For some reason, the nixpkgs flake doesn't roll its local
-  # lib.nixosSystem into nixpkgs.lib. We expose it here.
-  inherit (final.lib.hacknix-lib.flake.inputs.nixpkgs.lib) nixosSystem;
-
-  # nixosSystem is difficult to compose, and it's often useful to
-  # extend the modules declared in a given configuration; e.g., to
-  # override one or more module definitions. This function makes it
-  # possible to add extra modules to a configuration.
-  nixosSystem' = extraModules: config:
-    nixosSystem (config // {
-      modules = (config.modules or [ ]) ++ extraModules;
-    });
-
   # Filter a package set so that only packages whose platform(s)
   # attribute contain `system` are in the output set.
   #
@@ -26,38 +13,78 @@ let
     in
     final.lib.filterAttrs supported pkgs;
 
-  nixosConfigurations = rec {
-    # Create an attrset of buildable nixosConfigurations, using any
-    # attribute in the `config.system.build` attrset. This is useful for
-    # building via a Nix Flake's `hydraJobs`.
-    #
-    # Originally from:
-    # https://github.com/Mic92/doctor-cluster-config/blob/d9964365bb112898fe2b4abb77a8408adf8b1cb5/flake.nix#L36
-    build' = attr: configurations:
-      final.lib.mapAttrs'
-        (name: config: final.lib.nameValuePair name config.config.system.build.${attr})
-        configurations;
+  # For some reason, the nixpkgs flake doesn't roll its local
+  # lib.nixosSystem into nixpkgs.lib. We expose it here.
+  inherit (final.lib.hacknix-lib.flake.inputs.nixpkgs.lib) nixosSystem;
 
-    # Build the `toplevel` attribute (e.g., something that can be
-    # deployed to a live system or container).
-    build = build' "toplevel";
+  # nixosSystem is difficult to compose, and it's often useful to
+  # extend the modules declared in a given configuration; e.g., to
+  # override one or more module definitions. This function makes it
+  # possible to add extra modules to a configuration.
+  nixosSystem' = extraModules: config:
+    nixosSystem (config // {
+      modules = (config.modules or [ ]) ++ extraModules;
+    });
 
-    # Build the `amazonImage` attribute.
-    buildAmazonImages = build' "amazonImage";
+  # Import a directory full of
+  # nixosConfigurations/darwinConfigurations and apply a function that
+  # has the same shape as nixosSystem'.
+  importFromDirectory = nixosSystemFn: dir: args:
+    final.lib.mapAttrs
+      (_: cf:
+        let config = cf args;
+        in nixosSystemFn config)
+      (final.lib.sources.importDirectory dir);
 
-    # Build the `isoImage` attribute.
-    buildISOImages = build' "isoImage";
-  };
+  # Create an attrset of buildable nixosConfigurations, using any
+  # attribute in the `config.system.build` attrset. This is useful for
+  # building via a Nix Flake's `hydraJobs`.
+  #
+  # Originally from:
+  # https://github.com/Mic92/doctor-cluster-config/blob/d9964365bb112898fe2b4abb77a8408adf8b1cb5/flake.nix#L36
+  build' = attr: configurations:
+    final.lib.mapAttrs'
+      (name: config: final.lib.nameValuePair name config.config.system.build.${attr})
+      configurations;
+
+  # Build the `toplevel` attribute (e.g., something that can be
+  # deployed to a live system or container).
+  build = build' "toplevel";
+
+  # Build the `amazonImage` attribute.
+  buildAmazonImages = build' "amazonImage";
+
+  # Build the `isoImage` attribute.
+  buildISOImages = build' "isoImage";
+
+  # Export nix-darwin's lib.darwinSystem function.
+  inherit (final.lib.hacknix-lib.flake.inputs.nix-darwin.lib) darwinSystem;
+
+  # Like nixosSystem' for darwinSystem.
+  darwinSystem' = extraModules: config:
+    darwinSystem (config // {
+      modules = (config.modules or [ ]) ++ extraModules;
+    });
 
 in
 {
   lib = (prev.lib or { }) // {
     flakes = (prev.lib.flakes or { }) // {
-      inherit nixosSystem nixosSystem';
-
       inherit filterPackagesByPlatform;
 
-      nixosConfigurations = (prev.lib.flakes.nixosConfigruations or { }) // nixosConfigurations;
+      inherit nixosSystem nixosSystem';
+
+      nixosConfigurations = (prev.lib.flakes.nixosConfigruations or { }) // {
+        inherit importFromDirectory;
+        inherit build' build buildAmazonImages buildISOImages;
+      };
+
+      inherit darwinSystem darwinSystem';
+
+      darwinConfigurations = (prev.lib.flakes.darwinConfigurations or { }) // {
+        inherit importFromDirectory;
+        inherit build' build;
+      };
     };
   };
 }
