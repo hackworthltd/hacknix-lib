418931cc3ff9a8fa229f76cd8e82e532fccdb7da
Drew Hess <src@drewhess.com>
2020-12-31 16:11:28 +0000

    Refactor the flake's overlay.
    Smash the old `overlays` attribute into a single overlay, as this is
    much easier to deal with downstream.

--- nix/overlays/111-lib-sources.nix
+++ nix/overlays/111-lib-sources.nix
@@ -143,6 +143,38 @@ let
       inherit owner repo rev narHash sha256 url;
     };
 
+  ## Useful for importing whole directories.
+  ##
+  ## Thanks to dtzWill:
+  ## https://github.com/dtzWill/nur-packages/commit/f601a6b024ac93f7ec242e6e3dbbddbdcf24df0b#diff-a013e20924130857c649dd17226282ff
+
+  listDirectory = action: dir:
+    let
+      list = builtins.readDir dir;
+      names = builtins.attrNames list;
+      allowedName = baseName: !(
+        # From lib/sources.nix, ignore editor backup/swap files
+        builtins.match "^\\.sw[a-z]$" baseName != null
+        || builtins.match "^\\..*\\.sw[a-z]$" baseName != null
+        || # Otherwise it's good
+        false
+      );
+      filteredNames = builtins.filter allowedName names;
+    in
+    builtins.listToAttrs (
+      builtins.map
+        (
+          name: {
+            name = builtins.replaceStrings [ ".nix" ] [ "" ] name;
+            value = action (dir + ("/" + name));
+          }
+        )
+        filteredNames
+    );
+  importDirectory = listDirectory import;
+  pathDirectory = listDirectory (d: d);
+  mkCallDirectory = callPkgs: listDirectory (p: callPkgs p { });
+
 in
 {
   lib = (prev.lib or { }) // {
@@ -170,6 +202,8 @@ in
       inherit cleanPackage;
 
       inherit gitHubFlakeAttrs;
+
+      inherit listDirectory pathDirectory importDirectory mkCallDirectory;
     };
   };
 }
